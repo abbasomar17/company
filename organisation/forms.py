96b3f5f7d8c07d6bbe7ec708400744dab2d1a1a2
abbasomar17 <abbasoa17@gmail.com>
2025-08-15 19:25:58 -0700

    Update forms.py

--- organisation/forms.py
+++ organisation/forms.py
@@ -116,7 +116,6 @@ class AddLawyerForm(forms.Form):
     address = forms.CharField(label="Address", max_length=50, widget=forms.TextInput(attrs={"class": "form-control"}))
 
 
-
 class EditLawyerForm(forms.Form):
     email = forms.EmailField(label="Email", max_length=50, widget=forms.EmailInput(attrs={"class": "form-control"}))
     password = forms.CharField(label="Password", max_length=50,
